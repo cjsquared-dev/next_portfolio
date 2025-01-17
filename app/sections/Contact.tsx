da0b811d60444b2300602f415ca4003936a5ab2b
cjsquared-dev <camjohnson0618@gmail.com>
2025-01-17 03:46:41 -0700

    fixed contact me section

--- app/sections/Contact.tsx
+++ app/sections/Contact.tsx
@@ -1,9 +1,9 @@
 export default function Contact() {
   return (
-    <section id="contact" className="min-h-screen flex flex-col justify-center items-center text-white py-8">
+    <div id="contact" className="flex flex-col justify-center items-center text-white py-8">
       <h1 className="text-3xl font-bold mb-2">Contact Me</h1>
         <p>Email: camjohnson0618@gmail.com</p>
-      </section>
+      </div>
     );
   }
   
