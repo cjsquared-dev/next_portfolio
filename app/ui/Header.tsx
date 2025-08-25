e7d7613361c3eae51f22b15ac6a21f83872fdcbd
cjsquared-dev <camjohnson0618@gmail.com>
2025-08-25 17:16:06 -0600

    updated header and resume

--- app/ui/Header.tsx
+++ app/ui/Header.tsx
@@ -4,7 +4,7 @@ const Header: React.FC = () => {
   return (
     <header className="text-center py-8 bg-gradient-to-r from-indigo-500 via-purple-500 to-indigo-500 text-white">
       <h1 className="text-5xl font-bold mb-2">Chris Johnson</h1>
-      <h2 className="text-2xl font-medium">Full Stack Web Developer</h2>
+      <h2 className="text-2xl font-medium">Full Stack Web Developer + Information Security Professional</h2>
     </header>
   );
 };
